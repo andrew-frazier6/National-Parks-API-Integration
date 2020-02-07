223a284d66a809bc1b36e04d8ad8e37f885a4b6b
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-07 11:18:30 -0500

    made hamburger work

--- function.js
+++ function.js
@@ -1,1 +1,27 @@
-console.log("testing");
+// because the burger is closed we're setting it to false as to whether it has
+// dropped down or not
+let toggleStatus = false;
+
+let toggleHamburger = function() {
+  let getHamburger = document.querySelector(".hamburger-dropdown");
+  let getCategories = document.querySelector(".hamburger-dropdown ul");
+  let getCategoriesTitle = document.querySelector(".hamburger-dropdown span");
+  let getCategoriesLinks = document.querySelectorAll(".hamburger-dropdown a");
+  if (toggleStatus === false) {
+    getCategories.style.visibility = "visible";
+    getHamburger.style.width = "300px";
+    getCategoriesTitle.style.opacity = "1";
+    for (let i = 0; i < length.getCategoriesLinks; i++) {
+      getCategoriesLinks[i].style.opacity = "1";
+    }
+    toggleStatus = true;
+  } else if (toggleStatus === true) {
+    getCategories.style.visibility = "hidden";
+    getHamburger.style.width = "50px";
+    getCategoriesTitle.style.opacity = "0";
+    for (let i = 0; i < length.getCategoriesLinks; i++) {
+      getCategoriesLinks[i].style.opacity = "0";
+    }
+    toggleStatus = false;
+  }
+};
