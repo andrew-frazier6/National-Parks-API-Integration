cab4d0a76f031cf9699c3960ba77edbf6666b6b4
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-09 17:42:07 -0500

    add data to buttons

--- function.js
+++ function.js
@@ -1,18 +1,58 @@
-let state = "id";
-let parkCodes = ["yose", "acad", "grte", "brca", "gaar", "yell"];
+// let state = "id";
+// let parkCodes = ["yose", "acad", "grte", "brca", "gaar", "yell"];
 // Yosemite, Acadia, Grand Teton, Bryce Canyon, Gates of the Artic, Yellowstone
-axios
-  .get(
-    `https://developer.nps.gov/api/v1/parks?&api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC&stateCode=${state}`
-  )
-  .then(res => {
-    console.log(res.data);
-  })
-  .catch(err => {
-    console.log("Error with the Search");
-  });
 
+// yosemite
+//developer.nps.gov/api/v1/parks?parkCode=yose&api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC
+// acadia
+// https: //developer.nps.gov/api/v1/parks?parkCode=acad&api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC
+// Grand Teton
+// https: // "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC"
 //
+
+// function yosemite() {
+//   let url = "https://developer.nps.gov/api/v1/parks?parkCode=";
+//   let parkCode = "yose";
+//   let apiKey =
+//     "&fields=description&api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC";
+//   let descriptionBox = document.querySelector(".description");
+
+//   fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}${apiKey}`)
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(res => {
+//       console.log("success with the Search", res.data[0].description);
+//       descriptionBox.innerHTML = res.data[0].description;
+//     })
+//     .catch(err => {
+//       console.log("nope", err);
+//     });
+// }
+
+let description = document.querySelector(".description");
+let link = document.querySelector(".link");
+let title = document.querySelector(".title");
+
+function render(parkCode, selector, link, title) {
+  let url = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&fields=description&api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      selector.innerHTML = res.data[0].description;
+      link.href = res.data[0].url;
+      title.innerHTML = res.data[0].fullName;
+    });
+}
+
+const yosemite = () => render("yose", description, link, title);
+const acadia = () => render("acad", description, link, title);
+const teton = () => render("grte", description, link, title);
+const canyon = () => render("brca", description, link, title);
+const artic = () => render("gaar", description, link, title);
+const yellowstone = () => render("yell", description, link, title);
+
 let toggleStatus = false;
 
 let toggleHamburger = function() {
